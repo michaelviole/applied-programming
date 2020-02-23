b8126775b447104928cec6ee95c0661a7ed27b9e
michaelviole <michaelviole@gmail.com>
2020-02-23 23:11:53 +0000

    dice commit

--- dice.rb
+++ dice.rb
@@ -4,9 +4,11 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-  "<h1>Hello, world!</h1>"
+  @die1 = rand(1..6) 
+  @die2 = rand(1..6)
+  view "dice"
 end
 
-get "/tacos" do
-    "Yes please"
-end
+# get "/tacos" do
+#     "Yes please"
+# end
